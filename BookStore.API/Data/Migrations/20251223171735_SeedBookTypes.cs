01042738ff0f0359aff2f46c4db7353ab2a7ced9
Mariusz Jacek <marios.yaceg@gmail.com>
2025-12-23 19:57:28 +0100

    Migration seeds

---
+++ BookStore.API/Data/Migrations/20251223171735_SeedBookTypes.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace BookStore.API.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedBookTypes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "BookTypes",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Science Fiction" },
+                    { 2, "Fantasy" },
+                    { 3, "Mystery" },
+                    { 4, "Non-Fiction" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "BookTypes",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "BookTypes",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "BookTypes",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "BookTypes",
+                keyColumn: "Id",
+                keyValue: 4);
+        }
+    }
+}
